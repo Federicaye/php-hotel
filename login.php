143536ff828c1c987a72a0cb76c0372ca469ac7b
Federica Gallo <gallofederica58@gmail.com>
2024-05-05 12:44:13 +0200

    error message

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php 
 include __DIR__ . "/Controller/auth.php";
+
 ?>
 
 <!DOCTYPE html>
@@ -8,15 +9,18 @@ include __DIR__ . "/Controller/auth.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH' crossorigin='anonymous'>
 </head>
 <body>
+    <?php  if (!empty($_GET['error']))
+    { echo "<div class='alert alert-danger'> Email o password errati </div>"; }; ?>
 <div class="d-flex justify-content-center align-items-center">
         <form id="loginform" action="login.php" method="POST">
-            <img class="mb-4" src="./images/mobile-logo.png" alt="logo" width="100">
+    
             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
             <div class="form-floating mb-4">
-                <input type="email" class="form-control" id="email" placeholder="name@example.com" name="email">
+                <input type="text" class="form-control" id="email" placeholder="name@example.com" name="email">
                 <label for="email">Email address</label>
             </div>
             <div class="form-floating mb-4">
